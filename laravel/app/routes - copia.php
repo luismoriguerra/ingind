08c5c44f4b6444ac164b8fe927b9f462dbf9e0af
Juan Rojas <carlos34343434@gmail.com>
2015-03-11 10:27:45 -0500

    test pc

---
+++ laravel/app/routes - copia.php
@@ -0,0 +1,36 @@
+<?php
+
+Route::get(
+    '/', function () {
+        return View::make('login');
+    }
+);
+
+Route::get(
+    'salir', function () {
+        Auth::logout();
+
+        return Redirect::to('/');
+    }
+);
+
+Route::controller('check', 'LoginController');
+
+Route::get(
+    '/{ruta}', function ($ruta) {
+    $val = explode("_", $ruta);
+        $valores = array( 'valida_ruta_url' => $ruta );
+            if (count($val) == 2) {
+                $dv = explode("=", $val[1]);
+                $valores[$dv[0]] = $dv[1];
+            }
+
+            return View::make($ruta)->with($valores);
+    }
+);
+
+Route::controller('language', 'LanguageController');
+
+Route::controller('cargo', 'CargoController');
+Route::controller('menu', 'MenuController');
+Route::controller('opcion', 'OpcionController');
