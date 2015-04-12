ee3795b23facec2117381335f703b7c86e8199ba
jsalcedo <jorgeshevchenk@gmail.com>
2015-04-12 13:49:15 -0500

    act

---
+++ laravel/app/views/admin/ruta/js/validar_ajax.php
@@ -0,0 +1,121 @@
+<script type="text/javascript">
+var Asignar={
+    Relacion:function(evento){
+
+        $.ajax({
+            url         : 'tabla_relacion/relacion',
+            type        : 'POST',
+            cache       : false,
+            dataType    : 'json',
+            beforeSend : function() {                
+                $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
+            },
+            success : function(obj) {
+                if(obj.rst==1){                    
+                    evento(obj.datos);
+                }  
+                $(".overlay,.loading-img").remove();
+            },
+            error: function(){
+                $(".overlay,.loading-img").remove();
+                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
+                                        '<i class="fa fa-ban"></i>'+
+                                        '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                        '<b><?php echo trans("greetings.mensaje_error"); ?></b>'+
+                                    '</div>');
+            }
+        });
+    },
+    mostrarRutaFlujo:function(datos,evento){
+
+        $.ajax({
+            url         : 'tabla_relacion/rutaflujo',
+            type        : 'POST',
+            cache       : false,
+            dataType    : 'json',
+            data        : datos,
+            beforeSend : function() {
+                $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
+            },
+            success : function(obj) {
+                if(obj.rst==1){
+                    evento(obj.datos);
+                }  
+                $(".overlay,.loading-img").remove();
+            },
+            error: function(){
+                $(".overlay,.loading-img").remove();
+                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
+                                        '<i class="fa fa-ban"></i>'+
+                                        '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                        '<b><?php echo trans("greetings.mensaje_error"); ?></b>'+
+                                    '</div>');
+            }
+        });
+    },
+    guardarRelacion:function(){
+        var datos=$("#form_tabla_relacion").serialize().split("txt_").join("").split("slct_").join("").split("_modal").join("");
+        $.ajax({
+            url         : 'tabla_relacion/guardar',
+            type        : 'POST',
+            cache       : false,
+            dataType    : 'json',
+            data        : datos,
+            beforeSend : function() {                
+                $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
+            },
+            success : function(obj) {
+                if(obj.rst==1){
+                    $('#asignarModal .modal-footer [data-dismiss="modal"]').click();
+                    SeleccionRelacion(obj.id,obj.codigo);
+                    Asignar.Relacion(RelacionHTML);
+                }  
+                $(".overlay,.loading-img").remove();
+            },
+            error: function(){
+                $(".overlay,.loading-img").remove();
+                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
+                                        '<i class="fa fa-ban"></i>'+
+                                        '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                        '<b><?php echo trans("greetings.mensaje_error"); ?></b>'+
+                                    '</div>');
+            }
+        });
+    },
+    guardarAsignacion:function(){
+        var datos=$("#form_asignar").serialize().split("txt_").join("").split("slct_").join("").split("_modal").join("");
+        $.ajax({
+            url         : 'ruta/crear',
+            type        : 'POST',
+            cache       : false,
+            dataType    : 'json',
+            data        : datos,
+            beforeSend : function() {
+                $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
+            },
+            success : function(obj) {
+                if(obj.rst==1){
+                    $("#tb_ruta_flujo").html("");
+                    $("#form_asignar input[type='hidden']").remove();
+                    $("#form_asignar input[type='text'],#form_asignar select").val("");
+                    $('#form_asignar select').multiselect('refresh');
+                    $("#msj").html('<div class="alert alert-dismissable alert-success">'+
+                                        '<i class="fa fa-check"></i>'+
+                                        '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                        '<b>'+obj.msj+'</b>'+
+                                    '</div>');
+                }  
+                $(".overlay,.loading-img").remove();
+            },
+            error: function(){
+                $(".overlay,.loading-img").remove();
+                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
+                                        '<i class="fa fa-ban"></i>'+
+                                        '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                        '<b><?php echo trans("greetings.mensaje_error"); ?></b>'+
+                                    '</div>');
+            }
+        });
+    }
+}
+</script>
