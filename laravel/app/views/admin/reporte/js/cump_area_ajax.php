8784cb3b4ee9e434159b39680cf1b8873a6022ce
juan rojas <carlos34343434@gmail.com>
2015-05-25 07:18:00 -0500

    repor

--- laravel/app/views/admin/reporte/js/cump_area_ajax.php
+++ laravel/app/views/admin/reporte/js/cump_area_ajax.php
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-var Consulta, ConsultaDetalle, dataMorris=[];
+var Consulta, ConsultaDetalle, ConsultaDetalle2;
 var CumpArea={
     mostrar:function(flujo_id, area_id){
         $.ajax({
@@ -29,9 +29,36 @@ var CumpArea={
             }
         });
     },
-    mostrarDetalle:function(ruta_id){
+    mostrarDetalle:function(ruta_flujo_id){
         $.ajax({
             url         : 'reporte/cumpareadetalle',
+            type        : 'POST',
+            cache       : false,
+            dataType    : 'json',
+            data        : {ruta_flujo_id:ruta_flujo_id},
+            beforeSend : function() {
+                $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
+            },
+            success : function(obj) {
+                $(".overlay,.loading-img").remove();
+                if(obj.rst==1){
+                    HTMLreporteDetalle(obj.datos);
+                    ConsultaDetalle=obj;
+                }
+            },
+            error: function(){
+                $(".overlay,.loading-img").remove();
+                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
+                                    '<i class="fa fa-ban"></i>'+
+                                    '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                    '<b>Ocurrio una interrupción en el proceso,Favor de intentar nuevamente. Si el problema persiste favor de comunicarse a ubicame@puedesencontrar.com</b>'+
+                                '</div>');
+            }
+        });
+    },
+    mostrarDetalle2:function(ruta_id){
+        $.ajax({
+            url         : 'reporte/cumpareadetalle2',
             type        : 'POST',
             cache       : false,
             dataType    : 'json',
@@ -42,8 +69,8 @@ var CumpArea={
             success : function(obj) {
                 $(".overlay,.loading-img").remove();
                 if(obj.rst==1){
-                    HTMLreporteDetalle(obj.datos);
-                    ConsultaDetalle=obj;
+                    HTMLreporteDetalle2(obj.datos);
+                    ConsultaDetalle2=obj;
                 }
             },
             error: function(){
