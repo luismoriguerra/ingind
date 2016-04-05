3a69220e24574121c3ab272177a9ec2566890573
shevchenko <jorgeshevchenk@gmail.com>
2016-04-05 14:29:57 -0500

    act

--- laravel/app/models/Referido.php
+++ laravel/app/models/Referido.php
@@ -3,4 +3,16 @@
 class Referido extends Base
 {
     public $table = "referidos";
+
+    public function getReferido(){
+        DB::beginTransaction();
+        
+
+        DB::commit();
+
+        return  array(
+                    'rst'=>1,
+                    'msj'=>'Registro realizado con éxito'
+        );
+    }
 }
