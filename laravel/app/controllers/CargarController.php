fc702c2875b74ac10e44587312ecb6e5a8d178bd
shevchenko <jorgeshevchenk@gmail.com>
2016-04-05 13:57:14 -0500

    act final

--- laravel/app/controllers/CargarController.php
+++ laravel/app/controllers/CargarController.php
@@ -45,7 +45,7 @@ class CargarController extends BaseController
                     for ($j=0; $j < count($detfile); $j++) { 
                         $buscar=array(chr(13).chr(10), "\r\n", "\n", "\r","\n\n","\xEF","\xBB","\xBF");
                         $reemplazar="";
-                        $detfile[$j]=str_replace($buscar,$reemplazar,$detfile[$j]);
+                        $detfile[$j]=trim(str_replace($buscar,$reemplazar,$detfile[$j]));
                         $array[$i][$j]=$detfile[$j];
                     }
 
