3441e09089d4c1457efcbbb21326aaf73b89c598
shevchenko <jorgeshevchenk@gmail.com>
2016-08-11 18:36:38 -0500

    act

--- laravel/app/config/database.php
+++ laravel/app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'm1ndepen_munind',
+			'database'  => 'm1ndepen_munind', 
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
