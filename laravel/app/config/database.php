7f7edf3b328721169e49eca20d50b196e12cd5ac
juan <carlos3434@hotmail.com>
2016-08-14 10:28:38 -0500

    database

--- laravel/app/config/database.php
+++ laravel/app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'procesos',
 			'username'  => 'root',
-			'password'  => 'carlos3434',
+			'password'  => 'juanLuna',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_general_ci',
 			'prefix'    => '',
