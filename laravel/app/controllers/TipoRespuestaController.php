587085a2fb211f9dd00a5ca49bbd254a5f8550c3
thorhan <paezvallejohecthor@gmail.com>
2016-08-14 21:54:17 -0500

    MANTENIMIENTO TIPORESPUESTAS

--- laravel/app/controllers/TipoRespuestaController.php
+++ laravel/app/controllers/TipoRespuestaController.php
@@ -1,22 +1,65 @@
 <?php
 
-class TipoRespuestaController extends \BaseController
+class TipoRespuestaController extends \BaseController 
 {
-
-    /**
-     * cargar menus, mantenimiento
-     * POST /tiporespuesta/cargar
-     *
-     * @return Response
-     */
     public function postCargar()
     {
-        //si la peticion es ajax
         if ( Request::ajax() ) {
-            $tipoRespuesta = TipoRespuesta::get(Input::all());
-            return Response::json(array('rst'=>1,'datos'=>$tipoRespuesta));
+            /*********************FIJO*****************************/
+            $array=array();
+            $array['where']='';$array['usuario']=Auth::user()->id;
+            $array['limit']='';$array['order']='';
+            
+            if (Input::has('draw')) {
+                if (Input::has('order')) {
+                    $inorder=Input::get('order');
+                    $incolumns=Input::get('columns');
+                    $array['order']=  ' ORDER BY '.
+                                      $incolumns[ $inorder[0]['column'] ]['name'].' '.
+                                      $inorder[0]['dir'];
+                }
+
+                $array['limit']=' LIMIT '.Input::get('start').','.Input::get('length');
+                $aParametro["draw"]=Input::get('draw');
+            }
+            /************************************************************/
+
+            if( Input::has("nombre") ){
+                $nombre=Input::get("nombre");
+                if( trim( $nombre )!='' ){
+                    $array['where'].=" AND tr.nombre LIKE '%".$nombre."%' ";
+                }
+            }
+
+            if( Input::has("tiempo") ){
+                $tiempo=Input::get("tiempo");
+                if( trim( $tiempo )!='' ){
+                    $array['where'].=" AND tr.tiempo='".$tiempo."' ";
+                }
+            }
+
+            if( Input::has("estado") ){
+                $estado=Input::get("estado");
+                if( trim( $estado )!='' ){
+                    $array['where'].=" AND tr.estado='".$estado."' ";
+                }
+            }
+
+            $array['order']=" ORDER BY tr.nombre ";
+
+            $cant  = TipoRespuesta::getCargarCount( $array );
+            $aData = TipoRespuesta::getCargar( $array );
+
+            $aParametro['rst'] = 1;
+            $aParametro["recordsTotal"]=$cant;
+            $aParametro["recordsFiltered"]=$cant;
+            $aParametro['data'] = $aData;
+            $aParametro['msj'] = "No hay registros aún";
+            return Response::json($aParametro);
+
         }
     }
+
     /**
      * cargar flujos, mantenimiento
      * POST /tiporespuesta/listar
--- laravel/app/views/admin/mantenimiento/js/tiporespuestas_ajax.php
+++ laravel/app/views/admin/mantenimiento/js/tiporespuestas_ajax.php
@@ -1,11 +1,9 @@
 <script type="text/javascript">
-var TipoRespuestas={
+
+var TipoRespuestas = {
     AgregarEditarTipoRespuesta:function(AE){
-        var datos=$("#form_flujo_tr").serialize().split("txt_").join("").split("slct_").join("");
-        var accion="tiporespuesta/crear";
-        if(AE==1){
-            accion="tiporespuesta/editar";
-        }
+        var datos = $("#form_tiporespuestas_modal").serialize().split("txt_").join("").split("slct_").join("");
+        var accion = (AE==1) ? "tiporespuesta/editar" : "tiporespuesta/crear";
 
         $.ajax({
             url         : accion,
@@ -16,35 +14,31 @@ var TipoRespuestas={
             beforeSend : function() {
                 $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
             },
-            success : function(obj) {                
-                $(".overlay,.loading-img").remove();
+            success : function(obj) {
+                $(".overlay, .loading-img").remove();
                 if(obj.rst==1){
-                    $('#t_tiporespuestas').dataTable().fnDestroy();
-
-                    TipoRespuestas.CargarTipoRespuestas(activarTabla);
-                    $("#msj").html('<div class="alert alert-dismissable alert-success">'+
-                                        '<i class="fa fa-check"></i>'+
-                                        '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
-                                        '<b>'+obj.msj+'</b>'+
-                                    '</div>');
+                    MostrarAjax('tiporespuestas');
+                    msjG.mensaje('success',obj.msj,4000);
                     $('#tiporespuestaModal .modal-footer [data-dismiss="modal"]').click();
-                }
-                else{ 
-                    $.each(obj.msj,function(index,datos){                        
-                        $("#error_"+index).attr("data-original-title",datos);
-                        $('#error_'+index).css('display','');                                         
-                    });     
+
+                } else {
+                    var cont = 0;
+
+                    $.each(obj.msj, function(index, datos){
+                        cont++;
+                         if(cont==1){
+                            alert(datos[0]);
+                       }
+
+                    });
                 }
             },
             error: function(){
                 $(".overlay,.loading-img").remove();
-                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
-                                    '<i class="fa fa-ban"></i>'+
-                                    '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
-                                    '<b>Ocurrio una interrupción en el proceso,Favor de intentar nuevamente.'+
-                                '</div>');
+                msjG.mensaje('danger','<b>Ocurrio una interrupción en el proceso,Favor de intentar nuevamente.',4000);
             }
         });
+
     },
     CargarTipoRespuestas:function(evento){
         $.ajax({
@@ -88,45 +82,36 @@ var TipoRespuestas={
             }
         });
     },
-    CambiarEstadoTipoRespuestas:function(id,AD){
-        $("#form_flujo_tr").append("<input type='hidden' value='"+id+"' name='id'>");
-        $("#form_flujo_tr").append("<input type='hidden' value='"+AD+"' name='estado'>");
-        var datos=$("#form_flujo_tr").serialize().split("txt_").join("").split("slct_").join("");
+    CambiarEstadoTipoRespuestas: function(id, AD){
+        $("#form_tiporespuestas_modal").append("<input type='hidden' value='"+id+"' name='id'>");
+        $("#form_tiporespuestas_modal").append("<input type='hidden' value='"+AD+"' name='estado'>");
+        var datos = $("#form_tiporespuestas_modal").serialize().split("txt_").join("").split("slct_").join("");
         $.ajax({
             url         : 'tiporespuesta/cambiarestado',
             type        : 'POST',
             cache       : false,
             dataType    : 'json',
             data        : datos,
-            beforeSend : function() {                
+            beforeSend : function() {
                 $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
             },
             success : function(obj) {
-                $(".overlay,.loading-img").remove();
-                if(obj.rst==1){
-                    $('#t_tiporespuestas').dataTable().fnDestroy();
-                    TipoRespuestas.CargarTipoRespuestas(activarTabla);
-                    $("#msj").html('<div class="alert alert-dismissable alert-info">'+
-                                        '<i class="fa fa-info"></i>'+
-                                        '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
-                                        '<b>'+obj.msj+'</b>'+
-                                    '</div>');
+                $(".overlay, .loading-img").remove();
+
+                if (obj.rst==1) {
+                    MostrarAjax('tiporespuestas');
+                    msjG.mensaje('success',obj.msj,4000);
                     $('#tiporespuestaModal .modal-footer [data-dismiss="modal"]').click();
-                }
-                else{ 
-                    $.each(obj.msj,function(index,datos){
+                } else {
+                    $.each(obj.msj, function(index, datos) {
                         $("#error_"+index).attr("data-original-title",datos);
                         $('#error_'+index).css('display','');
-                    });     
+                    });
                 }
             },
             error: function(){
                 $(".overlay,.loading-img").remove();
-                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
-                                    '<i class="fa fa-ban"></i>'+
-                                    '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
-                                    '<b>Ocurrio una interrupción en el proceso,Favor de intentar nuevamente.'+
-                                '</div>');
+                msjG.mensaje('danger','<b>Ocurrio una interrupción en el proceso,Favor de intentar nuevamente.',4000);
             }
         });
     }
