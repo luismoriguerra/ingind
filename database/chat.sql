0569831e5e1916686a90d2945058fba78e74ab0e
juan <carlos3434@hotmail.com>
2016-09-12 23:15:58 -0500

    sql

--- database/chat.sql
+++ database/chat.sql
@@ -14,7 +14,7 @@ MySQL - 5.6.28 : Database - ingind
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-/USE `procesos`;
+
 
 /*Table structure for table `conversations` */
 
