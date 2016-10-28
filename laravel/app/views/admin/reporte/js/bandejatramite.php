1864ed60dc2dd9cb3ac4ff0ae4780622aa956b02
shevchenk <jorgeshevchenk@gmail.com>
2016-10-28 08:27:38 -0500

    actualizacion bandeja

--- laravel/app/views/admin/reporte/js/bandejatramite.php
+++ laravel/app/views/admin/reporte/js/bandejatramite.php
@@ -213,6 +213,8 @@ mostrarDetalleHTML=function(datos){
     var documento="";
     var orden="";
     var archivo="";
+
+    $("#t_detalle_verbo").html("");
         if ( datos.verbo!='' ) {
             detalle=datos.verbo.split("|");
             html="";
