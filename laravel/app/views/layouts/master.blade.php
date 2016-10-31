c3c943ad14c2a1597a60d37a2134307004f0c22a
williams solano francia <williams solano francia>
2016-10-31 12:48:44 -0500

    salida

--- laravel/app/views/layouts/master.blade.php
+++ laravel/app/views/layouts/master.blade.php
@@ -46,6 +46,78 @@
 
             <aside class="right-side">
             @yield('contenido')
+
+
+            <a class="open-chat-button tooltips" onClick="show()" href="javascript:void(0)" data-toggle="tooltip" data-placement="right" title="" data-original-title="Info Chat"><i class="fa fa-wechat"></i></a>
+                
+                <a class="open-chat-button-active tooltips" href="javascript:void(0)" data-toggle="tooltip" data-placement="right" title="" data-original-title="Info Chat"><i class="fa fa-wechat"></i></a>
+
+                <div class="live-chat">
+                    <form action="#" id="form-chat" class="sky-form">
+                            <header>Chat<button type="button" class="close-chat-button minimize-chat-button" aria-hidden="true"><i class="fa fa-minus"></i></button></header>                            
+                            <fieldset>                  
+                                <section>
+                                    <div class="row">
+	                                    <div class="col-md-3 col-sm-3 col-xs-3 imagenPerfil">
+	                                    	<img src="{{asset('img/user/u5.jpg')}}" style="height: 40px;width: 40px;border-radius: 50px">
+	                                    </div>
+	                                    <div class="col-md-9 col-sm-9 col-xs-9 datosPerfil">
+	                                    	<h3>Pedro Williams Solano Francia</h3>
+	                                    	<p>Analista Programador</p>
+	                                    </div>
+                                    </div>
+                                </section>
+
+                                <section>
+                                	<div class="col-md-12 col-sm-12 col-xs-12 left">
+                                        <div class="imagenPerfil">
+                                            <img src="http://www.e-quipu.pe/dinamic/perfil/63x75/avatar_vqrzZZ-H-5MsYAunGQzJdaTeC.jpg" class="img-circle" style="height:40px;width:40px">
+                                        </div>
+                                                                                  
+                                        <div class="datosPerfil">
+                                            <h3 class="nombre">
+                                               Fabio Franco Venero Carra
+                                            </h3>
+                                            <p>Analista Programador</p>
+                                        </div>
+                                    </div>
+                                </section>
+                                
+                               
+                            </fieldset>
+                            <footer>
+                                <button type="submit" class="btn-u">Enter</button>
+                            </footer>
+                        </form>
+                    <!-- Default Panel -->
+                    <div class="panel panel-u" style="display:none">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><i class="fa fa-comment"></i> Talk to us!
+                                <button type="button" class="close-chat-button" aria-hidden="true"><i class="fa fa-times"></i></button>
+                                <button type="button" class="minimize-chat-button" aria-hidden="true"><i class="fa fa-minus"></i></button>
+                            </h3>
+                        </div>
+                        <div class="panel-body c-body">
+                            <div class="alert alert-warning" style="margin:15px">
+                               <strong>Hello my dear!</strong> A coordinator will be attending in a few minutes. Just wait or leave us a message and we will soon answer...
+                            </div>                              
+                        </div>
+                        <div class="panel-footer">
+                            <form class="sky-form">
+                                <label class="input">
+                                    <button id="btn-send" class="btn-u icon-append"><i class="glyphicon glyphicon-play"></i></button>
+                                    <input type="hidden" id="chat_id">
+                                    <input type="text" id="in-message" placeholder="Type here...">
+                                </label>
+                            </form>
+                            
+                        </div>
+                    </div>
+                    <!-- End Default Panel -->
+                </div>
+
+
+
             </aside><!-- /.right-side -->
              
         </div><!-- ./wrapper -->
@@ -59,5 +131,9 @@
                 $(elemento).addClass('active');
             }
         });
+
+        show = function(){
+        	$('.live-chat').css('display', 'block');
+        }
     </script>
 </html>
