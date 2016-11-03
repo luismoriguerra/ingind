a10d3cbe7b35f469553c20c23d869dfb540248b5
williams solano francia <williams solano francia>
2016-11-03 18:16:43 -0500

    sss

--- laravel/app/views/templates/messages.blade.php
+++ laravel/app/views/templates/messages.blade.php
@@ -1,7 +1,7 @@
 <template v-for="message in messages">
     <div class="message">
         <div class="media msg ">
-            <small class="pull-right time"><i class="fa fa-clock-o"></i> @{{ message.created_at.date }}</small>
+            
             <a class="pull-left" href="#">
                 <img class="media-object img-circle" width="30" height="30" :src=" message.img ">
             </a>
