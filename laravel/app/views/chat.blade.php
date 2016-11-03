5de7e9f190096613345488a6a30abc32540a11cd
williams solano francia <williams solano francia>
2016-11-03 18:52:21 -0500

    sss

--- laravel/app/views/chat.blade.php
+++ laravel/app/views/chat.blade.php
@@ -65,7 +65,7 @@
                 <div class="row mensaje">
                     <div class="col-md-12 col-sm-12 col-xs-12 left">
                         <span>Mensaje:</span>
-                        <textarea v-model="body" rows="6" class="form-control" name="body"  id="body" style="border-radius: 5px !important"></textarea>
+                        <textarea v-model="body" rows="4" class="form-control" name="body"  id="body" style="border-radius: 5px !important"></textarea>
                     </div>
                 </div>
                 <div class="row enviar" style="margin-top:5px">
