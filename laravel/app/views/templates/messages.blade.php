5256222019b15f7081c92b5a1d07af3598b6ee81
williams solano francia <williams solano francia>
2016-11-03 18:27:57 -0500

    sss

--- laravel/app/views/templates/messages.blade.php
+++ laravel/app/views/templates/messages.blade.php
@@ -6,7 +6,7 @@
                 <img class="media-object img-circle" width="30" height="30" :src=" message.img ">
             </a>
             <div class="media-body">
-                <h3 class="media-heading">@{{ message.user_nombre+' ( '+message.area_nemonico+' )' }}</h3>
+                <label class="media-heading">@{{ message.user_nombre+' ( '+message.area_nemonico+' )' }}</label>
                 <small>@{{ message.body }}</small>
             </div>
         </div>
