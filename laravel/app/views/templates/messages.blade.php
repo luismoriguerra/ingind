b44696d0a10a7c75130388285031a0cfe25c67f3
williams solano francia <williams solano francia>
2016-11-03 18:24:28 -0500

    sss

--- laravel/app/views/templates/messages.blade.php
+++ laravel/app/views/templates/messages.blade.php
@@ -1,12 +1,12 @@
 <template v-for="message in messages">
-    <div class="message">
+    <div class="message" style="margin-bottom: 10px">
         <div class="media msg ">
             
             <a class="pull-left" href="#">
                 <img class="media-object img-circle" width="30" height="30" :src=" message.img ">
             </a>
             <div class="media-body">
-                <h5 class="media-heading">@{{ message.user_nombre+' ( '+message.area_nemonico+' )' }}</h5>
+                <h3 class="media-heading">@{{ message.user_nombre+' ( '+message.area_nemonico+' )' }}</h3>
                 <small>@{{ message.body }}</small>
             </div>
         </div>
