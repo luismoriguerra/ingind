0ceb7d09054c766c25eab8a82677264263c83519
shevchenk <jorgeshevchenk@gmail.com>
2016-11-04 22:21:58 -0500

    actualizacion de BD

--- database/query.sql
+++ database/query.sql
@@ -1,8 +1,322 @@
+--04-11-2016
+DROP TABLE IF EXISTS `tipo_tramite` ;
+
+CREATE  TABLE IF NOT EXISTS `tipo_tramite` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `nombre_tipo_tramite` VARCHAR(100) NULL ,
+  `estado` INT(11) NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT(11) NULL ,
+  `usuario_updated_at` INT(11) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+DROP TABLE IF EXISTS `clasificador_tramite` ;
+
+CREATE  TABLE IF NOT EXISTS `clasificador_tramite` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `area_id` INT(11) NOT NULL ,
+  `tipo_tramite_id` INT NOT NULL ,
+  `nombre_clasificador_tramite` VARCHAR(150) NULL ,
+  `estado` INT(11) NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT(11) NULL ,
+  `usuario_updated_at` INT(11) NULL ,
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_clasificador_tramite_areas1`
+    FOREIGN KEY (`area_id` )
+    REFERENCES `areas` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_clasificador_tramite_tipo_tramite1`
+    FOREIGN KEY (`tipo_tramite_id` )
+    REFERENCES `tipo_tramite` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_clasificador_tramite_areas1_idx` ON `clasificador_tramite` (`area_id` ASC) ;
+
+CREATE INDEX `fk_clasificador_tramite_tipo_tramite1_idx` ON `clasificador_tramite` (`tipo_tramite_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `empresa`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `empresa` ;
+
+CREATE  TABLE IF NOT EXISTS `empresa` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `tipo_id` INT NULL ,
+  `ruc` INT(11) NULL ,
+  `razon_social` VARCHAR(200) NULL ,
+  `nombre_comercial` VARCHAR(150) NULL ,
+  `direccion_fiscal` VARCHAR(250) NULL ,
+  `representante_legal` INT NULL COMMENT 'persona_id' ,
+  `cargo` VARCHAR(50) NULL ,
+  `telefono` VARCHAR(12) NULL ,
+  `fecha_vigencia` DATETIME NULL ,
+  `estado` INT(11) NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT NULL ,
+  `usuario_updated_at` INT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pretramites`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `pretramites` ;
+
+CREATE  TABLE IF NOT EXISTS `pretramites` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `persona_id` INT(11) NOT NULL ,
+  `clasificador_tramite_id` INT NOT NULL ,
+  `empresa_id` INT(11) NULL ,
+  `tipo_solicitante_id` INT NOT NULL ,
+  `tipo_documento_id` INT NOT NULL ,
+  `documento` VARCHAR(100) NULL ,
+  `nro_folios` INT NULL ,
+  `fecha_pretramite` DATETIME NULL ,
+  `estado` INT(11) NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT(11) NULL ,
+  `usuario_updated_at` INT(11) NULL ,
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_PRE TRAMITE_personas1`
+    FOREIGN KEY (`persona_id` )
+    REFERENCES `personas` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PRE_TRAMITE_clasificador_tramite1`
+    FOREIGN KEY (`clasificador_tramite_id` )
+    REFERENCES `clasificador_tramite` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PRE_TRAMITE_empresa1`
+    FOREIGN KEY (`empresa_id` )
+    REFERENCES `empresa` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_PRE TRAMITE_personas1_idx` ON `pretramites` (`persona_id` ASC) ;
+
+CREATE INDEX `fk_PRE_TRAMITE_clasificador_tramite1_idx` ON `pretramites` (`clasificador_tramite_id` ASC) ;
+
+CREATE INDEX `fk_PRE_TRAMITE_empresa1_idx` ON `pretramites` (`empresa_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `tipo_solicitante`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tipo_solicitante` ;
+
+CREATE  TABLE IF NOT EXISTS `tipo_solicitante` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT ,
+  `nombre` VARCHAR(50) NULL DEFAULT NULL ,
+  `nombre_relacion` VARCHAR(50) NULL DEFAULT NULL ,
+  `estado` INT(11) NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL DEFAULT NULL ,
+  `updated_at` DATETIME NULL DEFAULT NULL ,
+  `usuario_created_at` INT(11) NULL DEFAULT NULL ,
+  `usuario_updated_at` INT(11) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tramites`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tramites` ;
+
+CREATE  TABLE IF NOT EXISTS `tramites` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `pretramite_id` INT NOT NULL ,
+  `persona_id` INT NOT NULL ,
+  `clasificador_tramite_id` INT NOT NULL ,
+  `empresa_id` INT NULL ,
+  `tipo_solicitante_id` INT NOT NULL ,
+  `tipo_documento_id` INT NOT NULL ,
+  `documento` VARCHAR(100) NULL ,
+  `nro_folios` INT NULL ,
+  `fecha_tramite` DATETIME NULL ,
+  `observacion` VARCHAR(250) NULL ,
+  `imagen` VARCHAR(100) NULL ,
+  `estado` INT(11) NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT(11) NULL ,
+  `usuario_updated_at` INT(11) NULL ,
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_tramite_externo_clasificador_tramite1`
+    FOREIGN KEY (`clasificador_tramite_id` )
+    REFERENCES `clasificador_tramite` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tramite_externo_tipo_solicitante1`
+    FOREIGN KEY (`tipo_solicitante_id` )
+    REFERENCES `tipo_solicitante` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tramite_pretramite1`
+    FOREIGN KEY (`pretramite_id` )
+    REFERENCES `pretramites` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tp_persona_id`
+    FOREIGN KEY (`persona_id` )
+    REFERENCES `personas` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_empresa_id`
+    FOREIGN KEY (`empresa_id` )
+    REFERENCES `empresa` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_tramite_externo_clasificador_tramite1_idx` ON `tramites` (`clasificador_tramite_id` ASC) ;
+
+CREATE INDEX `fk_tramite_externo_tipo_solicitante1_idx` ON `tramites` (`tipo_solicitante_id` ASC) ;
+
+CREATE INDEX `fk_tramite_pretramite1_idx` ON `tramites` (`pretramite_id` ASC) ;
+
+CREATE INDEX `fk_tp_persona_id_idx` ON `tramites` (`persona_id` ASC) ;
+
+CREATE INDEX `fk_empresa_id_idx` ON `tramites` (`empresa_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `requisitos`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `requisitos` ;
+
+CREATE  TABLE IF NOT EXISTS `requisitos` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `clasificador_tramite_id` INT NOT NULL ,
+  `cantidad` INT(11) NULL ,
+  `nombre` VARCHAR(100) NULL ,
+  `estado` INT NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT(11) NULL ,
+  `usuario_updated_at` INT(11) NULL ,
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_REQUISITOS_clasificador_tramite1`
+    FOREIGN KEY (`clasificador_tramite_id` )
+    REFERENCES `clasificador_tramite` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_REQUISITOS_clasificador_tramite1_idx` ON `requisitos` (`clasificador_tramite_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `tramite_anexo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tramite_anexo` ;
+
+CREATE  TABLE IF NOT EXISTS `tramites_anexo` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `tramite_id` INT NOT NULL ,
+  `persona_id` INT NULL COMMENT 'Persona quien realiza el trámite del anexo' ,
+  `fecha_anexo` DATETIME NULL ,
+  `usuario_atendio` INT NULL COMMENT 'persona_id' ,
+  `nombre` VARCHAR(100) NULL ,
+  `nro_folios` INT NULL ,
+  `obeservacion` VARCHAR(250) NULL ,
+  `imagen` VARCHAR(100) NULL ,
+  `estado` INT NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT(11) NULL ,
+  `usuario_updated_at` INT(11) NULL ,
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_DetalleAnexo_tramite_externo1`
+    FOREIGN KEY (`tramite_id` )
+    REFERENCES `tramites` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_DetalleAnexo_tramite_externo1_idx` ON `tramite_anexo` (`tramite_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `empresa_persona`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `empresas_persona` ;
+
+CREATE  TABLE IF NOT EXISTS `empresa_persona` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `empresa_id` INT(11) NOT NULL ,
+  `persona_id` INT NULL COMMENT 'es la persona que podra realizar los tramites de la empresa seleccionada' ,
+  `cargo` VARCHAR(100) NULL ,
+  `fecha_vigencia` DATE NULL ,
+  `fecha_cese` DATE NULL ,
+  `estado` INT NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT NULL ,
+  `usuario_updated_at` INT NULL ,
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_empresa_persona_empresa1`
+    FOREIGN KEY (`empresa_id` )
+    REFERENCES `empresa` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_empresa_persona_empresa1_idx` ON `empresa_persona` (`empresa_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `tramites_requisitos`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tramites_requisitos` ;
+
+CREATE  TABLE IF NOT EXISTS `tramites_requisitos` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `tramite_id` INT NOT NULL ,
+  `cantidad` INT(11) NULL ,
+  `nombre` VARCHAR(100) NULL ,
+  `estado` INT NULL DEFAULT 1 ,
+  `created_at` DATETIME NULL ,
+  `updated_at` DATETIME NULL ,
+  `usuario_created_at` INT(11) NULL ,
+  `usuario_updated_at` INT(11) NULL ,
+  PRIMARY KEY (`id`) ,
+  CONSTRAINT `fk_tramites_id`
+    FOREIGN KEY (`tramite_id` )
+    REFERENCES `tramites` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_tramites_id_idx` ON `tramites_requisitos` (`tramite_id` ASC) ;
+
+/*******************************************************************************/
 --27-10-2016
 ALTER TABLE `rutas_detalle_verbo`
 ADD COLUMN `adicional`  int NULL DEFAULT 0 AFTER `estado`;
-
-
 
 --2016-10-20
 ALTER TABLE `referidos`
@@ -13,7 +327,6 @@ ADD COLUMN `ruta_detalle_verbo_id`  int(11) NULL AFTER `ruta_detalle_id`;
 
 ALTER TABLE `rutas_detalle`
 ADD COLUMN `motivo_edit`  varchar(255) NULL AFTER `observacion`;
-
 
 --2016-09-23
 ALTER TABLE `asignaciones`
