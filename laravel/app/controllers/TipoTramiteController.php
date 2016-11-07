115081a6dddbd67145f9a26b6daa60b2d248fdb5
thorhan <paezvallejohecthor@gmail.com>
2016-11-07 11:22:55 -0500

    MANTENIMIENTOTIPOTRAMITE

---
+++ laravel/app/controllers/TipoTramiteController.php
@@ -0,0 +1,192 @@
+<?php
+
+class TipoTramiteController extends \BaseController
+{
+    protected $_errorController;
+    /**
+     * Valida sesion activa
+     */
+    public function __construct(ErrorController $ErrorController)
+    {
+        $this->beforeFilter('auth');
+        $this->_errorController = $ErrorController;
+    }
+    /**
+     * cargar tipotramites, mantenimiento
+     * POST /rol/cargar
+     *
+     * @return Response
+     */
+    public function postCargar()
+    {
+        if ( Request::ajax() ) {
+            /*********************FIJO*****************************/
+            $array=array();
+            $array['where']='';$array['usuario']=Auth::user()->id;
+            $array['limit']='';$array['order']='';
+            
+            if (Input::has('draw')) {
+                if (Input::has('order')) {
+                    $inorder=Input::get('order');
+                    $incolumns=Input::get('columns');
+                    $array['order']=  ' ORDER BY '.
+                                      $incolumns[ $inorder[0]['column'] ]['name'].' '.
+                                      $inorder[0]['dir'];
+                }
+
+                $array['limit']=' LIMIT '.Input::get('start').','.Input::get('length');
+                $aParametro["draw"]=Input::get('draw');
+            }
+            /************************************************************/
+
+            if( Input::has("nombre") ){
+                $nombre=Input::get("nombre");
+                if( trim( $nombre )!='' ){
+                    $array['where'].=" AND tt.nombre LIKE '%".$nombre."%' ";
+                }
+            }
+
+            if( Input::has("estado") ){
+                $estado=Input::get("estado");
+                if( trim( $estado )!='' ){
+                    $array['where'].=" AND tt.estado='".$estado."' ";
+                }
+            }
+
+            $array['order']=" ORDER BY tt.nombre ";
+
+            $cant  = TipoTramite::getCargarCount( $array );
+            $aData = TipoTramite::getCargar( $array );
+
+            $aParametro['rst'] = 1;
+            $aParametro["recordsTotal"]=$cant;
+            $aParametro["recordsFiltered"]=$cant;
+            $aParametro['data'] = $aData;
+            $aParametro['msj'] = "No hay registros aún";
+            return Response::json($aParametro);
+
+        }
+    }
+    /**
+     * cargar tipotramites, mantenimiento
+     * POST /rol/listar
+     *
+     * @return Response
+     */
+   public function postListar()
+    {
+        if ( Request::ajax() ) {
+            $a      = new TipoTramite;
+            $listar = Array();
+            $listar = $a->getTipoTramite();
+
+            return Response::json(
+                array(
+                    'rst'   => 1,
+                    'datos' => $listar
+                )
+            );
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * POST /rol/crear
+     *
+     * @return Response
+     */
+
+    public function postCrear()
+    {
+        if ( Request::ajax() ) {
+            $regex = 'regex:/^([a-zA-Z .,ñÑÁÉÍÓÚáéíóú]{2,60})$/i';
+            $required = 'required';
+            $reglas = array(
+                'nombre' => $required.'|'.$regex,
+            );
+
+            $mensaje= array(
+                'required' => ':attribute Es requerido',
+                'regex'    => ':attribute Solo debe ser Texto',
+            );
+
+            $validator = Validator::make(Input::all(), $reglas, $mensaje);
+
+            if ( $validator->fails() ) {
+                return Response::json( array('rst'=>2, 'msj'=>$validator->messages()) );
+            }
+
+            $tipotramite = new TipoTramite;
+            $tipotramite->nombre = Input::get('nombre');
+            $tipotramite->estado = Input::get('estado');
+            $tipotramite->usuario_created_at = Auth::user()->id;
+            $tipotramite->save();
+
+            return Response::json(array('rst'=>1, 'msj'=>'Registro realizado correctamente', 'tipotramite_id'=>$tipotramite->id));
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * POST /rol/editar
+     *
+     * @return Response
+     */
+   public function postEditar()
+    {
+        if ( Request::ajax() ) {
+            $regex = 'regex:/^([a-zA-Z .,ñÑÁÉÍÓÚáéíóú]{2,60})$/i';
+            $required = 'required';
+            $reglas = array(
+                'nombre' => $required.'|'.$regex,
+            );
+
+            $mensaje= array(
+                'required' => ':attribute Es requerido',
+                'regex'    => ':attribute Solo debe ser Texto',
+            );
+
+            $validator = Validator::make(Input::all(), $reglas, $mensaje);
+
+            if ( $validator->fails() ) {
+                return Response::json( array('rst'=>2, 'msj'=>$validator->messages()) );
+            }
+
+            $tipotramiteId = Input::get('id');
+            $tipotramite = TipoTramite::find($tipotramiteId);
+            $tipotramite->nombre = Input::get('nombre');
+            $tipotramite->estado = Input::get('estado');
+            $tipotramite->usuario_updated_at = Auth::user()->id;
+            $tipotramite->save();
+
+            return Response::json(array('rst'=>1, 'msj'=>'Registro actualizado correctamente'));
+        }
+    }
+
+    /**
+     * Changed the specified resource from storage.
+     * POST /rol/cambiarestado
+     *
+     * @return Response
+     */
+    public function postCambiarestado()
+    {
+
+        if ( Request::ajax() ) {
+
+            $tipotramite = TipoTramite::find(Input::get('id'));
+            $tipotramite->usuario_created_at = Auth::user()->id;
+            $tipotramite->estado = Input::get('estado');
+            $tipotramite->save();
+           
+            return Response::json(
+                array(
+                'rst'=>1,
+                'msj'=>'Registro actualizado correctamente',
+                )
+            );    
+
+        }
+    }
+
+}
---
+++ laravel/app/models/TipoTramite.php
@@ -0,0 +1,47 @@
+<?php
+
+class TipoTramite extends Base
+{
+    public $table = "tipo_tramite";
+    public static $where =['id', 'nombre', 'estado'];
+    public static $selec =['id', 'nombre', 'estado'];
+    
+    public static function getCargarCount( $array )
+    {
+        $sSql=" SELECT  COUNT(tt.id) cant
+                FROM tipo_tramite tt
+                WHERE 1=1 ";
+        $sSql.= $array['where'];
+        $oData = DB::select($sSql);
+        return $oData[0]->cant;
+    }
+
+    public static function getCargar( $array )
+    {
+        $sSql=" SELECT tt.id, tt.nombre, tt.estado
+                FROM tipo_tramite tt
+                WHERE 1=1 ";
+        $sSql.= $array['where'].
+                $array['order'].
+                $array['limit'];
+        $oData = DB::select($sSql);
+        return $oData;
+    }
+
+
+    public function getTipoTramite(){
+        $tipotramite=DB::table('tipo_tramite')
+                ->select('id','nombre','estado')
+                ->where( 
+                    function($query){
+                        if ( Input::get('estado') ) {
+                            $query->where('estado','=','1');
+                        }
+                    }
+                )
+                ->orderBy('nombre')
+                ->get();
+                
+        return $tipotramite;
+    }
+}
