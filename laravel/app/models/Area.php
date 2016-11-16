fb1adf7844258dab5839cb4cf1840037fb3bcef3
williams solano francia <williams solano francia>
2016-11-16 15:49:51 -0500

    areas procesos

--- laravel/app/models/Area.php
+++ laravel/app/models/Area.php
@@ -97,7 +97,7 @@ class Area extends Base
                             foreach($areas as $key => $value){
                                 $areas_id.= ($key == 0) ? $value->area_id : ','.$value->area_id; 
                             }
-                            $query->whereRaw('FIND_IN_SET( id,('.$areas_id.'))>0 ');
+                            $query->whereRaw('FIND_IN_SET( id,'.$areas_id.')>0 ');
                         }
                     }
                 )
