fb10e0e0eb3ba331d7777d84d239f5f4320bb7a9
juan <carlos3434@hotmail.com>
2016-11-22 21:12:54 -0500

    ver lista de personas de misempresas vecino

--- laravel/app/views/admin/empresa/listado.blade.php
+++ laravel/app/views/admin/empresa/listado.blade.php
@@ -48,7 +48,7 @@
             <div>
                 <ul id="myTab" class="nav nav-tabs" role="tablist">
                     <li role="presentation" class="active">
-                        <a  href="#misempresas">Mis empresas</a>
+                        <a  href="#mis">Mis empresas</a>
                     </li>
                     <li role="presentation">
                         <a  href="#afiliadas">Empresas Afiliadas</a>
@@ -56,9 +56,10 @@
                 </ul>
             </div>
             <div class="tab-content">
-                <div class="tab-pane fade active in" id="misempresas">
-                    <spinner id="spinner-box" :size="size" :fixed="fixed" v-show="loaded" text="Espere un momento por favor"></spinner>
+                <div class="tab-pane fade active in" id="mis">
                     <div class="box box-solid">
+                    <div id="misempresas">
+                        <spinner id="spinner-box" :size="size" :fixed="fixed" v-show="loaded" text="Espere un momento por favor"></spinner>
                         <div class="alert alert-success" transition="success" v-if="success">@{{ msj }} </div>
 
                         <div class="form-group">
@@ -79,7 +80,7 @@
                             </div>
                         </div>
                         <div class="table-responsive">
-                            <vuetable v-ref:vuetable
+                            <vuetable v-ref:vuetable id="empresas"
                                 api-url="empresa"
                                 data-path="data"
                                 pagination-path=""
@@ -112,8 +113,54 @@
                             </div>
                         </div>
                         @include( 'admin.empresa.form.listado' ) 
+                    </div>
+                    <div id="personas">
+                        <div class="form-group">
+                            <div class="row form-group form-inline">
+                                <div class="col-md-6">
+                                    <div class="control-group">
+                                        <label class="control-label">Buscar:</label>
+                                        <input v-model="searchFor" class="form-control input-sm" @keyup.enter="setFilter">
+                                        <button class="btn btn-primary btn-sm" @click="setFilter">Go</button>
+                                        <button class="btn btn-default btn-sm" @click="resetFilter">Reset</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <vuetable v-ref:vuetable
+                                api-url="empresapersona/afiliados"
+                                data-path="data"
+                                pagination-path=""
+                                :fields="fields"
+                                :sort-order="sortOrder"
+                                :multi-sort="multiSort"
+                                table-class="table table-bordered table-striped table-hover"
+                                ascending-icon="glyphicon glyphicon-chevron-up"
+                                descending-icon="glyphicon glyphicon-chevron-down"
+                                pagination-class=""
+                                pagination-info-class=""
+                                pagination-component-class=""
+                                :pagination-component="paginationComponent"
+                                :item-actions="itemActions"
+                                :append-params="moreParams"
+                                :per-page="perPage"
+                                wrapper-class="vuetable-wrapper"
+                                table-wrapper=".vuetable-wrapper"
+                                loading-class="loading"
+                                detail-row-component="my-detail-row"
+                                detail-row-id="id"
+                                detail-row-transition="expand"
+                                row-class-callback="rowClassCB"
+                            ></vuetable>
+                        </div>
+                    </div>
+
                     </div><!-- /.box -->
                 </div>
+
+
+
                 <div class="tab-pane fade" id="afiliadas">
                     <spinner id="spinner-box" :size="size" :fixed="fixed" v-show="loaded" text="Espere un momento por favor"></spinner>
                     <div class="box box-solid">
@@ -174,6 +221,7 @@
     </div>
     @include( 'admin.empresa.js.misempresas' )
     @include( 'admin.empresa.js.afiliadas' )
+    @include( 'admin.empresa.js.personas_misempresas' )
 </section><!-- /.content -->
 @stop
 
