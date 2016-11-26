01dab208829fb5306a716cb926e1e78b477183de
Renzo CB <rcapchab@gmail.com>
2016-11-25 20:03:58 -0500

    Produccion por Usuario

--- laravel/app/views/admin/reporte/js/produccionusu.php
+++ laravel/app/views/admin/reporte/js/produccionusu.php
@@ -4,16 +4,31 @@ $(document).ready(function() {
         format: 'YYYY-MM-DD',
         singleDatePicker: false
     });
+    var dataG = {estado:1};
     var data = {estado:1};
     var ids = [];
     slctGlobal.listarSlct('area','slct_area_id','multiple',ids,data);
-    $("#generar").click(function (){
+    $("#generar_area").click(function (){
         area_id = $('#slct_area_id').val();
         if ($.trim(area_id)!=='') {
             data = {area_id:area_id};
             Usuario.mostrar(data);
         } else {
             alert("Seleccione Área");
+        }
+    });
+    
+    
+      $("#generar").click(function (){
+        usuario_id = $('#usuario_id').val();
+        var fecha=$("#fecha").val();
+        if ( fecha!=="") {
+                dataG = {usuario_id:usuario_id,fecha:fecha};
+                Usuario.CargarProduccion(dataG);
+                Usuario.CargarProduccionArea(dataG);
+       
+        } else {
+            alert("Seleccione Fecha");
         }
     });
    
@@ -52,6 +67,37 @@ HTMLreporte=function(datos){
     $("#reporte").show();
 };
 
+HTMLproxarea=function(datos){
+  var html="";
+    
+    var alerta_tipo= '';
+    $('#t_produccion').dataTable().fnDestroy();
+    pos=0;
+    $.each(datos,function(index,data){
+        pos++;
+        html+="<tr>"+
+            "<td>"+data.nombre+"</td>"+
+            "<td>"+data.tareas+"</td>";
+        html+="</tr>";
+    });
+    $("#tb_produccion").html(html);
+    $("#t_produccion").dataTable(
+    ); 
+    $("#produccion").show();
+
+};
+
+HTMLproduccion=function(datos){
+    var html="";
+    $.each(datos,function(index,data){
+      html+="<table class='table table-bordered'><tr><td><b>Total de Tareas Realizadas</b></td><td>"+data.tareas+"</td></tr>";
+    });
+    
+    $("#div_total_produccion").html(html);
+  
+};
+
+
 MostrarUsuario=function(id){
 
 
@@ -65,6 +111,7 @@ MostrarUsuario=function(id){
     //var apmm = $("tr td")[1].innerHTML;
     //var nombree = $("tr td")[2].innerHTML;
     $("#txt_persona").attr("value",app+" "+apm +" "+ nombre);
+    $("#usuario_id").attr("value",id);
     
 };
 
@@ -72,7 +119,7 @@ Regresar=function(){
     
     $('#reporte').show();
     $('fieldset').show();
-    
+     $("#produccion").hide();
     $('#bandeja_detalle').hide();
     
 };
