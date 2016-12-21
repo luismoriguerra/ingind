7564bebf25ba391dd92a516d92eccb9799b4b3ed
Pedro Williams Solano Francia <williams_98_francia@hotmail.com>
2016-12-21 11:47:47 -0500

    chat ok

--- laravel/app/views/templates/messages.blade.php
+++ laravel/app/views/templates/messages.blade.php
@@ -3,7 +3,7 @@
         <div class="media msg ">
             {{-- <small class="pull-right time"><i class="fa fa-clock-o"></i> @{{ message.created_at }}</small> --}}
             <a class="pull-left" href="#">
-                <img class="media-object img-circle" width="30" height="30" :src="message.img">
+                <img class="media-object img-circle" width="30" height="30" :src="message.img" data-toggle="tooltip" data-placement="bottom" title="{{ message.user.username }}">
             </a>
             <div class="media-body">
                 <span class="media-heading" style="font-size: 12px;font-weight: bold;">@{{ message.user.username }}</span>
