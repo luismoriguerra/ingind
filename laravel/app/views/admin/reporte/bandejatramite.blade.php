865c13d2051243392ce5292529bcd17290970f68
shevchenk <jorgeshevchenk@gmail.com>
2017-01-17 10:06:09 -0500

    act

--- laravel/app/views/admin/reporte/bandejatramite.blade.php
+++ laravel/app/views/admin/reporte/bandejatramite.blade.php
@@ -201,10 +201,10 @@ tr.shown td.details-control {
                                                 <i class="fa fa-search fa-lg"></i>
                                                 .::Expediente::.
                                             </a>
-                                           {{--  <a class="btn btn-sm btn-primary  hidden" id="RetornarP" onclick="retornar()">
+                                            <a class="btn btn-sm btn-primary  hidden" id="RetornarP" onclick="retornar()">
                                                 <i class="glyphicon glyphicon-repeat"></i>
                                                 Retornar Paso
-                                            </a> --}}
+                                            </a>
                                             </h1>
                                         </div>
                                         <div class="col-sm-12">
