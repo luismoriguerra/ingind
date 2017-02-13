8f200953fdf02d59271ea796142cdf9991c72cf3
shevchenk <jorgeshevchenk@gmail.com>
2017-02-12 23:21:49 -0500

    activacion

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -106,7 +106,6 @@ Route::group(['before' => 'auth'], function() {
 });
 Route::get(
     '/{ruta}', array('before' => 'auth', function ($ruta) {
-        return Redirect::to('/salir');
         if (Session::has('accesos')) {
             $accesos = Session::get('accesos');
             $menus = Session::get('menus');
