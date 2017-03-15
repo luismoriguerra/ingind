45bc40fb869d1c13fc07642d6a40cd8e2418f20a
Pedro Williams Solano Francia <williams_98_francia@hotmail.com>
2017-03-15 09:19:17 -0500

    ss

--- laravel/app/views/admin/ruta/js/pretramite.php
+++ laravel/app/views/admin/ruta/js/pretramite.php
@@ -331,7 +331,7 @@ selectEmpresa = function(obj){
 poblateData = function(tipo,data){
 /*    if(tipo == 'usuario'){*/
         document.querySelector('#txt_userdni').value='<?php echo Auth::user()->dni; ?>';
-        document.querySelector('#txt_usernomb').value=r'<?php echo Auth::user()->nombre; ?>';
+        document.querySelector('#txt_usernomb').value='<?php echo Auth::user()->nombre; ?>';
         document.querySelector('#txt_userapepat').value='<?php echo Auth::user()->paterno; ?>';
         document.querySelector('#txt_userapemat').value='<?php echo Auth::user()->materno; ?>';
     /*    user_telf.value=data.;
