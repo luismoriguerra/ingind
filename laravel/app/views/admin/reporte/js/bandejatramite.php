585b87231c369431587265a6b0979b72c2fdaf02
Renzo CB <rcapchab@gmail.com>
2017-04-05 15:54:25 -0500

    a

--- laravel/app/views/admin/reporte/js/bandejatramite.php
+++ laravel/app/views/admin/reporte/js/bandejatramite.php
@@ -265,9 +265,10 @@ mostrarDetallle=function(id,rtid = ''){ //OK
      $("#form_ruta_detalle>#ruta_id").remove();
     $("#form_ruta_detalle").append("<input type='hidden' id='ruta_id' name='ruta_id' value='"+rtid+"'>");
     var datos={ruta_detalle_id:id};
-   $("#slct_persona").multiselect('rebuild');
+   
     var dataG={estado_persona:1,area_documento:1,ruta_detalle_id:id};
     slctGlobal.listarSlct('persona','slct_persona','simple',null,dataG);
+    $("#slct_persona").multiselect('rebuild');
     Validar.mostrarDetalle(datos,mostrarDetalleHTML);
 }
 
