0e8786be1a1846ea94a5c08ef65567542a1af626
shevchenk <jorgeshevchenk@gmail.com>
2017-04-17 13:09:19 -0500

    actualizacion

--- laravel/app/models/DocumentoDigital.php
+++ laravel/app/models/DocumentoDigital.php
@@ -10,9 +10,9 @@ class DocumentoDigital extends Base {
         if(Input::get('id')){
             return DB::table('doc_digital as dd')
                     ->join('plantilla_doc as pd', 'dd.plantilla_doc_id', '=', 'pd.id')
-                    ->join('doc_digital_area as dda', 'dda.doc_digital_id', '=', 'dd.id')
-                    ->join('areas as a','dda.area_id', '=', 'a.id')
-                    ->join('personas as p','dda.persona_id', '=', 'p.id')
+                    ->leftjoin('doc_digital_area as dda', 'dda.doc_digital_id', '=', 'dd.id')
+                    ->leftjoin('areas as a','dda.area_id', '=', 'a.id')
+                    ->leftjoin('personas as p','dda.persona_id', '=', 'p.id')
                     ->select('dd.id', 'dd.titulo', 'dd.asunto', 'pd.descripcion as plantilla', 'dd.plantilla_doc_id' ,'a.nombre as area','dda.area_id as area_id','p.nombre as pnombre','p.paterno as ppaterno','p.materno as pmaterno','dd.cuerpo','dd.tipo_envio','dda.persona_id','dda.tipo','dd.envio_total')
                     ->where( 
 
