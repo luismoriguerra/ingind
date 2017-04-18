c837e0ea508a520701386ac2af584575bffc6adb
Renzo CB <rcapchab@gmail.com>
2017-04-18 12:19:42 -0500

    a

--- laravel/app/controllers/PlantillaDocumentoController.php
+++ laravel/app/controllers/PlantillaDocumentoController.php
@@ -80,6 +80,7 @@ class PlantillaDocumentoController extends \BaseController {
 
             $params = [
                 'nombre' => $Plantilladoc->nombre,
+                'area' => $Plantilladoc->area_id,
                 'conCabecera' => 1,
                 'contenido' => $Plantilladoc->cuerpo,
                 'titulo' =>  " N&ordm; 001-" . date('Y') . "-XX/XX"
