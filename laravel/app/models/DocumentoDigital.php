8ee66c43b8b590d4d588f972c3e6c72f1739e348
shevchenko <jorgeshevchenk@gmail.com>
2017-04-24 11:11:44 -0500

    actualizacion modelos

--- laravel/app/models/DocumentoDigital.php
+++ laravel/app/models/DocumentoDigital.php
@@ -24,14 +24,25 @@ class DocumentoDigital extends Base {
                             if ( Input::get('id') ) {
                                 $query->where('dd.id','=',Input::get('id'));
                             }
-                            $sql="  SELECT count(id) cant
-                                    FROM cargo_persona
-                                    WHERE estado=1
-                                    AND cargo_id=12
-                                    AND persona_id=".Auth::user()->id;
-                            $csql=DB::select($sql);
-                            if( $csql[0]->cant==0 ){
-                            $query->where('dd.area_id','=',Auth::user()->area_id);
+                            else{
+                                $usu_id=Auth::user()->id;
+                                $sql="  SELECT count(id) cant
+                                        FROM cargo_persona
+                                        WHERE estado=1
+                                        AND cargo_id=12
+                                        AND persona_id=".$usu_id;
+                                $csql=DB::select($sql);
+                                if( $csql[0]->cant==0 ){
+                                    //$query->where('dd.area_id','=',Auth::user()->area_id);
+                                    $query->whereRaw('dd.area_id IN (
+                                        SELECT DISTINCT(a.id)
+                                        FROM area_cargo_persona acp
+                                        INNER JOIN areas a ON a.id=acp.area_id AND a.estado=1
+                                        INNER JOIN cargo_persona cp ON cp.id=acp.cargo_persona_id AND cp.estado=1
+                                        WHERE acp.estado=1
+                                        AND cp.persona_id= '.$usu_id.'
+                                    )');
+                                }
                             }
                             $query->where('dd.estado','=',1);
                         }
@@ -54,6 +65,7 @@ class DocumentoDigital extends Base {
                             } else {
                                  $query->where('dd.estado','=','1');
                             }
+                            $usu_id=Auth::user()->id;
 /*                            $sql="  SELECT count(id) cant
                                     FROM cargo_persona
                                     WHERE estado=1
@@ -61,7 +73,15 @@ class DocumentoDigital extends Base {
                                     AND persona_id=".Auth::user()->id;
                             $csql=DB::select($sql);
                             if( $csql[0]->cant==0 ){*/
-                                $query->where('dd.area_id','=',Auth::user()->area_id);
+                                //$query->where('dd.area_id','=',Auth::user()->area_id);
+                                $query->whereRaw('dd.area_id IN (
+                                        SELECT DISTINCT(a.id)
+                                        FROM area_cargo_persona acp
+                                        INNER JOIN areas a ON a.id=acp.area_id AND a.estado=1
+                                        INNER JOIN cargo_persona cp ON cp.id=acp.cargo_persona_id AND cp.estado=1
+                                        WHERE acp.estado=1
+                                        AND cp.persona_id= '.$usu_id.'
+                                    )');
                             /* }*/
                         }
                     )
