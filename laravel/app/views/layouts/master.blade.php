316796c3ae9b6fdc7fdafb870f92a1357aa0ab35
Renzo CB <rcapchab@gmail.com>
2017-05-12 11:58:20 -0500

    a

--- laravel/app/views/layouts/master.blade.php
+++ laravel/app/views/layouts/master.blade.php
@@ -66,39 +66,48 @@
 	<script>
         var MensajeG9973=0;
         $(document).ready(function() { 
-        '<?php   $ssql="  SELECT m.id as m_id,m.contenido,m.titulo,md.id as dm_id
+        <?php   $ssql="  SELECT m.id as m_id,m.contenido,m.titulo,md.id as dm_id
                           FROM mensajes m
-                          LEFT JOIN mensajes_detalle md ON m.id=md.mensaje_id AND DATE(m.created_at)=CURDATE() AND md.usuario_created_at=".Auth::user()->id;
-                $mensaje= DB::select($ssql);?>';
+                          LEFT JOIN mensajes_detalle md ON m.id=md.mensaje_id AND md.usuario_created_at=".Auth::user()->id."
+			  WHERE DATE(m.created_at)=CURDATE() AND m.estado=1";
+        $mensaje= DB::select($ssql);
+        if(count($mensaje)==0){
+            $mensaje=array(
+                       (object) array('contenido'=>'','titulo'=>'','dm_id'=>'','m_id'=>'')
+                    );
+                    
+        }
+        ?>;
 
-        var contenido='<?php echo $mensaje[0]->contenido?>';
-        var titulo='<?php echo $mensaje[0]->titulo?>';
-        var dm_id='<?php echo $mensaje[0]->dm_id?>';    
-        var id='<?php echo $mensaje[0]->m_id?>';
-//        if(dm_id==null || dm_id==''){
-//                
-//                $('#mensajeModal').on('show.bs.modal', function (event) {
-//                    MensajeG9973=id;
-//                    $("#form_mensajes_modal #contenido").html(contenido);
-//                        var modal = $(this); //captura el modal
-//                        modal.find('.modal-title').text(titulo);
-//                        $('#form_mensajes_modal [data-toggle="tooltip"]').css("display","none");
-//                        $("#form_mensajes_modal input[type='hidden']").remove();
-//                        
-//                        modal.find('.modal-footer .btn-primary').text('Confirmar Lectura');
-//                        modal.find('.modal-footer .btn-primary').attr('onClick','RegistrarMensajeVisto('+MensajeG9973+');');
-//                        
-//                });
-//
-//                $('#mensajeModal').on('hide.bs.modal', function (event) {
-////               $('#form_mensajes_modal input').val('');
-//                });
-//                $("#mensajeModal").modal({
-//                        keyboard: false,
-//                        backdrop: false
-//                    });
-//             
-//        }
+        var contenido='<?php  echo $mensaje[0]->contenido?>';
+        var titulo='<?php  echo $mensaje[0]->titulo?>';
+        var dm_id='<?php  echo $mensaje[0]->dm_id?>';    
+        var id='<?php   echo $mensaje[0]->m_id?>';
+                        
+        if( (dm_id==null || dm_id=='') && id!='' ){
+                
+                $('#mensajeModal').on('show.bs.modal', function (event) {
+                    MensajeG9973=id;
+                    $("#form_mensajes_modal #contenido").html(contenido);
+                        var modal = $(this); //captura el modal
+                        modal.find('.modal-title').text(titulo);
+                        $('#form_mensajes_modal [data-toggle="tooltip"]').css("display","none");
+                        $("#form_mensajes_modal input[type='hidden']").remove();
+                        
+                        modal.find('.modal-footer .btn-primary').text('Confirmar Lectura');
+                        modal.find('.modal-footer .btn-primary').attr('onClick','RegistrarMensajeVisto('+MensajeG9973+');');
+                        
+                });
+
+                $('#mensajeModal').on('hide.bs.modal', function (event) {
+//               $('#form_mensajes_modal input').val('');
+                });
+                $("#mensajeModal").modal({
+                        keyboard: false,
+                        backdrop: false
+                    });
+             
+        }
         });   
      /*   $('.').perfectScrollbar();*/
         $('.myscroll').perfectScrollbar({
