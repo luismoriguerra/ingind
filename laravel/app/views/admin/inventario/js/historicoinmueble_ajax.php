dd26027a17e95e1fdf7c30679707fd8ae402ba50
Renzo CB <rcapchab@gmail.com>
2017-07-13 17:31:59 -0500

    a

---
+++ laravel/app/views/admin/inventario/js/historicoinmueble_ajax.php
@@ -0,0 +1,59 @@
+<script type="text/javascript">
+var Accion={
+    mostrar:function( data){
+        $.ajax({
+            url         : 'reporte/historicoinventario',
+            type        : 'POST',
+            cache       : false,
+            dataType    : 'json',
+            data        : data,
+            beforeSend : function() {
+                $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
+            },
+            success : function(obj) {
+                $(".overlay,.loading-img").remove();
+                if(obj.rst==1){
+                    HTMLreporte(obj.datos);
+                    Consulta=obj;
+                }
+            },
+            error: function(){
+                $(".overlay,.loading-img").remove();
+                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
+                                    '<i class="fa fa-ban"></i>'+
+                                    '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                    '<b>Ocurrio una interrupción en el proceso,Favor de intentar nuevamente.'+
+                                '</div>');
+            }
+        });
+    },
+    export:function(data){
+        $.ajax({
+            url         : 'reporte/exportnotincumplimiento',
+            type        : 'GET',
+            cache       : false,
+            dataType    : 'json',
+            data        : data,
+            beforeSend : function() {
+                $("body").append('<div class="overlay"></div><div class="loading-img"></div>');
+            },
+            success : function(obj) {
+                $(".overlay,.loading-img").remove();
+                if(obj.rst==1){
+                  /*  HTMLreporte(obj.datos);
+                    Consulta=obj;*/
+                }
+            },
+            error: function(){
+                $(".overlay,.loading-img").remove();
+                $("#msj").html('<div class="alert alert-dismissable alert-danger">'+
+                                    '<i class="fa fa-ban"></i>'+
+                                    '<button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>'+
+                                    '<b>Ocurrio una interrupción en el proceso,Favor de intentar nuevamente.'+
+                                '</div>');
+            }
+        });
+    }
+
+};
+</script>
