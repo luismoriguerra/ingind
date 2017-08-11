721079a049991c368cc1fdee4e184d33b9519f64
thorhan <paezvallejohecthor@gmail.com>
2017-08-11 17:27:01 -0500

    TICKETPROGMGM

--- laravel/app/views/admin/ruta/js/ticketgmgm.php
+++ laravel/app/views/admin/ruta/js/ticketgmgm.php
@@ -2,7 +2,7 @@
 var cabeceraG=[]; // Cabecera del Datatable
 var columnDefsG=[]; // Columnas de la BD del datatable
 var targetsG=-1; // Posiciones de las columnas del datatable
-var TicketsG={id:0,persona_id:"",nombre_solicitante:"",area_id:"",nombre_solicitante_area:"",descripcion:"",fecha_pendiente:"",fecha_atencion:"",fecha_solucion:"",estado_ticket:1,estado:1}; // Datos Globales
+var TicketsgmgmG={id:0,persona_id:"",nombre_solicitante:"",area_id:"",nombre_solicitante_area:"",descripcion:"",fecha_pendiente:"",fecha_atencion:"",fecha_solucion:"",estado_ticket:1,estado:1}; // Datos Globales
 $(document).ready(function() {  
     /*  1: Onblur ,Onchange y para número es a travez de una función 1: 
         2: Descripción de cabecera
@@ -25,16 +25,16 @@ $(document).ready(function() {
 
     var resG=dataTableG.CargarCab(idG);
     cabeceraG=resG; // registra la cabecera
-    var resG=dataTableG.CargarCol(cabeceraG,columnDefsG,targetsG,1,'tickets','t_tickets');
+    var resG=dataTableG.CargarCol(cabeceraG,columnDefsG,targetsG,1,'tickets','t_ticketgmgms');
     columnDefsG=resG[0]; // registra las columnas del datatable
     targetsG=resG[1]; // registra los contadores
-    //var resG=dataTableG.CargarBtn(columnDefsG,targetsG,1,'BtnEditar','t_tickets','fa-edit');
+    //var resG=dataTableG.CargarBtn(columnDefsG,targetsG,1,'BtnEditar','t_ticketgmgms','fa-edit');
     //columnDefsG=resG[0]; // registra la colunmna adiciona con boton
     //targetsG=resG[1]; // registra el contador actualizado
-    MostrarAjax('tickets');
+    MostrarAjax('ticketgmgms');
 
 
-    $('#ticketModal').on('show.bs.modal', function (event) {
+    $('#ticketgmgmModal').on('show.bs.modal', function (event) {
 
         /*$('#txt_fecha_nacimiento').daterangepicker({
             format: 'YYYY-MM-DD',
@@ -49,39 +49,39 @@ $(document).ready(function() {
       // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
       // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
       var modal = $(this); //captura el modal
-      modal.find('.modal-title').text(titulo+' Ticket');
-      $('#form_tickets_modal [data-toggle="tooltip"]').css("display","none");
-      $("#form_tickets_modal input[type='hidden']").not('.mant').remove();
+      modal.find('.modal-title').text(titulo+' Ticket GMGM');
+      $('#form_ticketgmgms_modal [data-toggle="tooltip"]').css("display","none");
+      $("#form_ticketgmgms_modal input[type='hidden']").not('.mant').remove();
 
         if(titulo=='Nuevo'){ 
 
             modal.find('.modal-footer .btn-primary').text('Guardar');
             modal.find('.modal-footer .btn-primary').attr('onClick','Agregar();');
-           // $('#form_tickets_modal #slct_estado_ticket').val(1); 
-           // $('#form_tickets_modal #txt_fecha_solucion').focus();
-            //$('#form_tickets_modal #txt_responsable').focus();
+           // $('#form_ticketgmgms_modal #slct_estado_ticket').val(1); 
+           // $('#form_ticketgmgms_modal #txt_fecha_solucion').focus();
+            //$('#form_ticketgmgms_modal #txt_responsable').focus();
 
         }else {
 
             modal.find('.modal-footer .btn-primary').text('Actualizar');
             modal.find('.modal-footer .btn-primary').attr('onClick','Editar();');
-            $('#form_tickets_modal #txt_persona_id').val( TicketsG.persona_id );
-            $('#form_tickets_modal #nombre_solicitante').val( TicketsG.nombre_solicitante );
-            $('#form_tickets_modal #nombre_solicitante_area').val( TicketsG.nombre_solicitante_area );
-            $('#form_tickets_modal #txt_area_id').val( TicketsG.area_id );
-            $('#form_tickets_modal #txt_fecha_pendiente').val( TicketsG.fecha_pendiente );
+            $('#form_ticketgmgms_modal #txt_persona_id').val( TicketsgmgmG.persona_id );
+            $('#form_ticketgmgms_modal #nombre_solicitante').val( TicketsgmgmG.nombre_solicitante );
+            $('#form_ticketgmgms_modal #nombre_solicitante_area').val( TicketsgmgmG.nombre_solicitante_area );
+            $('#form_ticketgmgms_modal #txt_area_id').val( TicketsgmgmG.area_id );
+            $('#form_ticketgmgms_modal #txt_fecha_pendiente').val( TicketsgmgmG.fecha_pendiente );
 
-            $('#form_tickets_modal #txt_descripcion').val( TicketsG.descripcion );
-            $("#form_tickets_modal").append("<input type='hidden' value='"+TicketsG.id+"' name='id'>");
+            $('#form_ticketgmgms_modal #txt_descripcion').val( TicketsgmgmG.descripcion );
+            $("#form_ticketgmgms_modal").append("<input type='hidden' value='"+TicketsgmgmG.id+"' name='id'>");
         }
 
-        $('#form_tickets_modal select').multiselect('rebuild');
+        $('#form_ticketgmgms_modal select').multiselect('rebuild');
         
     });
 
-    $('#ticketModal').on('hide.bs.modal', function (event) {
+    $('#ticketgmgmModal').on('hide.bs.modal', function (event) {
 
-        $('#form_tickets_modal #txt_descripcion').val( '' );
+        $('#form_ticketgmgms_modal #txt_descripcion').val( '' );
         
 
 
@@ -89,7 +89,7 @@ $(document).ready(function() {
 });
 
 MostrarAjax=function(t){
-    if( t=="tickets" ){
+    if( t=="ticketgmgms" ){
         if( columnDefsG.length>0 ){
             dataTableG.CargarDatos(t,'ticket','cargar',columnDefsG);
         }
@@ -113,13 +113,14 @@ GeneraFn=function(row,fn){ // No olvidar q es obligatorio cuando queire funcion 
         //estado_tickethtml='<span id="'+row.id+'" onClick="atendido('+row.id+')" data-estado_ticket="'+row.estado_ticket+'" class="btn btn-success">Atendido</span>';
        
             if(row.estado_ticket==1){
-                estado_tickethtml='<span id="'+row.id+'"  data-estado_ticket="'+row.estado_ticket+'" >Pendiente</span>';
+                estado_tickethtml='<span id="'+row.id+'" onClick="CambiarEstado('+row.id+',2)" data-estado_ticket="'+row.estado_ticket+'" class="btn btn-success" >Pendiente</span>';
+
             }
             else if(row.estado_ticket==2){
-                estado_tickethtml='<span id="'+row.id+'"  data-estado_ticket="'+row.estado_ticket+'" >Atendido</span>';
+                estado_tickethtml='<span id="'+row.id+'" onClick="CambiarEstado('+row.id+',3)" data-estado_ticket="'+row.estado_ticket+'" class="btn btn-warning" >Atendido</span>';
             }
             else if(row.estado_ticket==3){
-                estado_tickethtml='<span id="'+row.id+'"  data-estado_ticket="'+row.estado_ticket+'" >Solucionado</span>';
+                estado_tickethtml='<span id="'+row.id+'" data-estado_ticket="'+row.estado_ticket+'" >Solucionado</span>';
             }
         return estado_tickethtml;
     }
@@ -127,7 +128,7 @@ GeneraFn=function(row,fn){ // No olvidar q es obligatorio cuando queire funcion 
     else if(typeof(fn)!='undefined' && fn.col==7){
      if(row.estado_ticket==1)
          {
-           return "<a class='btn btn-primary btn-sm' onClick='BtnEditar(this,"+row.id+")' data-toggle='modal' data-target='#ticketModal' return false;' data-titulo='Editar'><i class='glyphicon glyphicon-pencil'></i> </a>";  
+           return "<a class='btn btn-primary btn-sm' onClick='BtnEditar(this,"+row.id+")' data-toggle='modal' data-target='#ticketgmgmModal' return false;' data-titulo='Editar'><i class='glyphicon glyphicon-pencil'></i> </a>";  
          }
     }
 
@@ -144,54 +145,54 @@ BtnEliminar = function(id){
     if(c)
         {
             data={'estado':0,'id':id};
-            Tickets.EliminarTicket(data);
+            Ticketgmgms.EliminarTicket(data);
         }
 };
 
 BtnEditar=function(btn,id){
     var tr = btn.parentNode.parentNode; // Intocable
-    TicketsG.id=id;
-    TicketsG.nombre_solicitante=$(tr).find("td:eq(0)").text();
-    TicketsG.nombre_solicitante_area=$(tr).find("td:eq(1)").text();
-    TicketsG.persona_id=$(tr).find("td:eq(0) input[name='txt_persona_id']").val();
-    TicketsG.area_id=$(tr).find("td:eq(1)").text();
-    TicketsG.descripcion=$(tr).find("td:eq(2)").text();
-    TicketsG.fecha_pendiente=$(tr).find("td:eq(3)").text();
+    TicketsgmgmG.id=id;
+    TicketsgmgmG.nombre_solicitante=$(tr).find("td:eq(0)").text();
+    TicketsgmgmG.nombre_solicitante_area=$(tr).find("td:eq(1)").text();
+    TicketsgmgmG.persona_id=$(tr).find("td:eq(0) input[name='txt_persona_id']").val();
+    TicketsgmgmG.area_id=$(tr).find("td:eq(1)").text();
+    TicketsgmgmG.descripcion=$(tr).find("td:eq(2)").text();
+    TicketsgmgmG.fecha_pendiente=$(tr).find("td:eq(3)").text();
     $("#BtnEditar").click();
 };
 
 
 activarTabla=function(){
-    $("#t_tickets").dataTable(); // inicializo el datatable    
+    $("#t_ticketgmgms").dataTable(); // inicializo el datatable    
 };
 
 activar=function(id){
-    Tickets.CambiarEstadoTickets(id,1);
+    Ticketgmgms.CambiarEstadoTickets(id,1);
 };
 
 desactivar=function(id){
-    Tickets.CambiarEstadoTickets(id,0);
+    Ticketgmgms.CambiarEstadoTickets(id,0);
 };
 
 Editar=function(){
     if(validaTickets()){
-        Tickets.AgregarEditarTicket(1);
+        Ticketgmgms.AgregarEditarTicket(1);
     }
 };
 
 Agregar=function(){
     if(validaTickets()){
-        Tickets.AgregarEditarTicket(0);
+        Ticketgmgms.AgregarEditarTicket(0);
     }
 };
 
 validaTickets=function(){
     var r=true;
-    if( $("#form_tickets_modal #txt_persona_id").val()=='' ){
+    if( $("#form_ticketgmgms_modal #txt_persona_id").val()=='' ){
         alert("Ingrese Nombre de Solicitante");
         r=false;
     }
-    if( $("#form_tickets_modal #txt_area_id").val()=='' ){
+    if( $("#form_ticketgmgms_modal #txt_area_id").val()=='' ){
         alert("Ingrese Area");
         r=false;
     }
@@ -202,7 +203,7 @@ validaTickets=function(){
 CambiarEstado=function(id,valor){
     
 
-        Tickets.CambiarEstadoTickets(id,valor);
+        Ticketgmgms.CambiarEstadoTickets(id,valor);
     
 };
 
