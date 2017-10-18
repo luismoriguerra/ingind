7ac7e7f16ac4dd7a530c8168df57acaeba658897
Renzo CB <rcapchab@gmail.com>
2017-10-18 16:42:23 -0500

    a

--- laravel/app/controllers/DocumentoDigitalController.php
+++ laravel/app/controllers/DocumentoDigitalController.php
@@ -55,9 +55,11 @@ class DocumentoDigitalController extends \BaseController {
             
             if( Input::has("created_at") ){
                 $created_at=Input::get("created_at");
-                list($fechaIni,$fechaFin) = explode(" - ", $created_at);
+                //list($fechaIni,$fechaFin) = explode(" - ", $created_at);
                 if( trim( $created_at )!='' ){
-                    $array['where'].=" AND DATE(dd.created_at) BETWEEN '".$fechaIni."' AND '".$fechaFin."'";
+                   // $array['where'].=" AND DATE(dd.created_at) BETWEEN '".$fechaIni."' AND '".$fechaFin."'";
+                      $array['where'].=" AND DATE(dd.created_at)= '".$created_at."'";
+                    
                 }
             }
             
