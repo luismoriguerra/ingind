eac1d3ad77ef01eb6b61f9281bd467c99aa24ef2
Renzo CB <rcapchab@gmail.com>
2017-10-20 16:35:08 -0500

    a

---
+++ laravel/app/models/AuditoriaAcceso.php
@@ -0,0 +1,23 @@
+<?php
+
+class AuditoriaAcceso extends \Eloquent {
+
+    public $table = "auditoria_acceso";
+    
+    public  static function getAuditoria(){
+        
+        $ruta=explode('.',URL::previous());
+        if(Request::path()!='admin.inicio' and (Auth::user()->rol_id==8 or Auth::user()->rol_id==9)){
+            $opcion= Opcion::where('ruta','like','%'.$ruta[2])->first();
+            
+            $auditoria=new AuditoriaAcceso;
+            $auditoria->persona_id=Auth::user()->id;
+            $auditoria->rol_id=Auth::user()->rol_id;
+            $auditoria->opcion_id=@$opcion->id;
+            $auditoria->tipo=2;
+            $auditoria->usuario_created_at=Auth::user()->id;
+            $auditoria->ruta=$ruta[2];
+            $auditoria->save();
+        }
+    }
+}
