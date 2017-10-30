e4aedd15a7b3be9002513dbba36660c445e92926
Renzo CB <rcapchab@gmail.com>
2017-10-30 17:19:10 -0500

    a

--- laravel/app/models/AuditoriaAcceso.php
+++ laravel/app/models/AuditoriaAcceso.php
@@ -16,7 +16,7 @@ class AuditoriaAcceso extends \Eloquent {
             $auditoria->opcion_id=@$opcion->id;
             $auditoria->tipo=2;
             $auditoria->usuario_created_at=Auth::user()->id;
-            $auditoria->ruta=$ruta[4];
+            $auditoria->ruta=$ruta;
             $auditoria->save();
         }
     }
