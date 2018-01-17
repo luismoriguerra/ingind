0da1193e229ca7f07d287b58277a0ed2331c9cb6
shevchenko <jorgeshevchenk@gmail.com>
2018-01-17 18:45:15 -0500

    cambios

--- laravel/app/models/Reporte.php
+++ laravel/app/models/Reporte.php
@@ -867,7 +867,6 @@ class Reporte extends Eloquent
                  AND rd.condicion = 0  ".
                 $array['ruta_flujo_id'].
                 $array['fecha'].
-                //$array['tramite'].
                 " GROUP BY rd.area_id
                   ORDER BY rd.area_id ";
 
