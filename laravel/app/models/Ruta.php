7d3571e62c4fc049a68eeefc2ca166938136c289
Renzo CB <rcapchab@gmail.com>
2018-03-21 14:00:47 -0500

    ruta_flujo_id_dep

--- laravel/app/models/Ruta.php
+++ laravel/app/models/Ruta.php
@@ -76,7 +76,7 @@ class Ruta extends Eloquent
                                 $rutaDetalle['area_id'] = $rfd->area_id;
                                 $rutaDetalle['tiempo_id'] = $rfd->tiempo_id;
                                 $rutaDetalle['dtiempo'] = $rfd->dtiempo;
-//                                $rutaDetalle['ruta_flujo_id']=$rd->ruta_flujo_id2;
+                                $rutaDetalle['ruta_flujo_id_dep']=$rdm->ruta_flujo_id;
                                 $rutaDetalle['norden'] = $rd->norden.'.'.$cero.$rfd->norden;
                                 $rutaDetalle['estado_ruta'] = $rfd->estado_ruta;
                                 $rutaDetalle['usuario_created_at'] = Auth::user()->id;
