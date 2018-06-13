4edfd2a29b3e3c4319328522b7cc5b7c5c6fc1f7
Jhoubert Rincon <jhoubert@veflat.com>
2018-06-13 12:05:00 -0500

    test2

--- laravel/app/controllers/AsignacionController.php
+++ laravel/app/controllers/AsignacionController.php
@@ -63,8 +63,9 @@ class AsignacionController extends \BaseController
     {
         //si la peticion es ajax
         if ( Request::ajax()){
-            
-            ini_set('memory_limit', '256MB');
+
+            ini_set('memory_limit', '512MB');
+            ini_set('display_errors', true);
 
             $norden = Input::get('norden');
             $mFile = Input::get('image');
