aaefc9556e9dab4d276228bd5c8aeb10d9fc74f6
Jhoubert Rincon <jhoubert@veflat.com>
2018-06-18 15:25:41 -0500

    Agregada expresion regular para detectar sí existen documentos en el asunto de la actividad, prueba 1

--- laravel/app/controllers/DocumentoDigitalController.php
+++ laravel/app/controllers/DocumentoDigitalController.php
@@ -1366,25 +1366,54 @@ public function getDoccargo($norden)
 
         $documentos = "";
 
+
+        preg_match_all('/(^|)(([Dd]([\ -]|)[Ss])|([Ee]([\ -]|)[Xx]))(-|\ |)([0-9]{4,10})([-|\ ][0-9]{4}|)(,|.|$|)/', $oData, $documentosEnAsunto);
+
+
+        $cantidadAux = count($documentosEnAsunto[0]);
+
+        if($cantidadAux == 0){
+
+        }else{
+            
+        }
+
+
+
         $docs = DocumentoDigital::actividadDocList($norden);
-        $cantidadDocs = count($docs);
+        $cantidadDocs = count($docs)+$cantidadAux;
 
 
         $i=1;
+
+
+
+
+
         if($cantidadDocs<=0){
+
             $asuntoAux = $oData[0]->actividad;
+
             $documentos = "<br><b>No hay documentos asignados.</b>";
             $notaDeCargo="Mediante el presente formulario se deja constancia que se hace entrega de los documentos entregados que se mencionan a continuación: <br>
     $asuntoAux
  <br><br>";
             $asunto = "Entrega de documento";
 
+
+
         }else{
 
-            foreach ($docs as $key => $value) {
-                $documentos .= "<br>&nbsp; ".$i++." - &nbsp;<b>".$docs[$key]->titulo."</b>";
 
+
+            if(($cantidadDocs-$cantidadAux)>0)foreach ($docs as $key => $value) {
+                $documentos .= "<br>&nbsp; ".$i++." - &nbsp;<b>".$value->titulo."</b>";
             }
+            if($cantidadAux>0)foreach ($documentosEnAsunto as $key => $value) {
+                $documentos .= "<br>&nbsp; ".$i++." - &nbsp;<b>".$value."</b>";
+            }
+
+
             $asunto = $oData[0]->actividad;
             $notaDeCargo="Mediante el presente formulario se deja constancia que se hace entrega de $cantidadDocs documentos, los documentos entregados se mencionan a continuación:<br>
 
